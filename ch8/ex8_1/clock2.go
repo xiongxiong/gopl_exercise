762d7afc4b99f72defeb16b1b134b9730df87f27
wjr <ximengwuheng@163.com>
2018-11-12 18:06:27 +0800

    m

---
+++ ch8/ex8_1/clock2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+var zones = []string{"CST", "MST", "PST", "AKST"}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("usage: clock 8080 8081 ...")
+	}
+
+	for i, port := range os.Args[1:] {
+		go serveTime(port, zones[i%4])
+	}
+
+	select {}
+}
+
+func serveTime(port string, zone string) {
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn, zone)
+	}
+}
+
+func handleConn(c net.Conn, zone string) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format(fmt.Sprintf("15:04:05 %s %s | ", zone, os.Getenv("TZ"))))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ ch8/ex8_1/clockwall/clockwall.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: clockwall NewYork=localhost:8010 Tokyo=localhost:8020 ...")
+	}
+
+	smap = make(map[int]string, len(os.Args)-1)
+
+	for i, arg := range os.Args[1:] {
+		pair := strings.Split(arg, "=")
+		go fetchTime(pair[1], i)
+	}
+
+	select {}
+}
+
+func fetchTime(address string, index int) {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	if _, err := io.Copy(&MyWriter{Index: index}, conn); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// MyWriter ...
+type MyWriter struct {
+	Index int
+}
+
+var mutex = sync.RWMutex{}
+
+var smap map[int]string
+
+// Write ...
+func (w *MyWriter) Write(b []byte) (int, error) {
+	mutex.Lock()
+	smap[w.Index] = string(b)
+	s := fmt.Sprintf("\r%s\t%s\t%s", smap[0], smap[1], smap[2])
+	mutex.Unlock()
+	if _, err := io.WriteString(os.Stdout, s); err != nil {
+		return len(b), err
+	}
+	return len(b), nil
+}
